aa740482c667975f2de4821c6756413b8bcd1c43
bdpoagdorado1 <bdpoagdorado1@gmail.com>
2022-01-30 11:54:55 -0600

    Initial commit

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,38 @@
+import { TestBed, async } from "@angular/core/testing";
+import { AppComponent } from "./app.component";
+
+describe("AppComponent", () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [AppComponent]
+    }).compileComponents();
+  }));
+
+  it("should create the app", () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'codesandbox-angular-testing'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual("codesandbox-angular-testing");
+  });
+
+  it("should render link containing the words", () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector("a").textContent).toContain("is so fun");
+  });
+
+  it("should render link matching the exact words", () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector("a").textContent).toBeTruthy(
+      "Angular testing is so fun, right?"
+    );
+  });
+});
